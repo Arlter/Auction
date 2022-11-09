493c8fc01361f0dc4385aa9ce81d0f73d39c6706
angellam99 <115711996+angellam99@users.noreply.github.com>
2022-11-09 00:05:33 +0000

    221108
    worked on process_registrtion.php

--- process_registration.php
+++ process_registration.php
@@ -4,4 +4,110 @@
 // an account. Notify user of success/failure and redirect/give navigation 
 // options.
 
+include "connection.php";
+
+//Input extraction
+if (isset($_POST["submit"])) {
+    $accountType = $_POST["accountType"];
+    $username = mysqli_real_escape_string ($conn, $_POST["username"]);
+    $password = mysqli_real_escape_string ($conn, $_POST["password"]);
+    $passwordConfirmation = mysqli_real_escape_string ($conn, $_POST["passwordConfirmation"]);
+    $firstName = mysqli_real_escape_string ($conn, $_POST["firstName"]);
+    $lastName = mysqli_real_escape_string ($conn, $_POST["lastName"]);
+    $email = mysqli_real_escape_string ($conn, $_POST["email"]);
+    $phoneNumber = mysqli_real_escape_string ($conn, $_POST["phoneNumber"]);
+}
+// alternative data cleaning: trim() then stripslashes() then htmlspecialchars()
+
+// test
+printf($username);
+echo "<br>";
+printf($password);
+echo "<br>";
+printf($passwordConfirmation);
+echo "<br>";
+printf($firstName);
+echo "<br>";
+printf($lastName);
+echo "<br>";
+printf($email);
+echo "<br>";
+printf($phoneNumber);
+echo "<br>";
+printf($accountType);
+echo "<br>";
+
+
+
+
+// Input validiation
+// what is the best structure/hierarchy?
+
+// check for empty input
+if (empty($username) || empty($password) || empty($passwordConfirmation) || empty($firstName) ||
+empty($lastName) || empty($email) || empty($phoneNumber)
+|| ctype_space($username) || ctype_space($password) || ctype_space($passwordConfirmation) || ctype_space($firstName) ||
+ctype_space($lastName) || ctype_space($email) || ctype_space($phoneNumber)) {
+    $error = "Please fill in all the required information";
+    header("Location: register.php?error =" . urlencode ($error));
+        
+    // add a closable alert message box
+    // might wanna set different alerts for different errors? 
+    // separate checks for each input, or group them into one statement?
+    // also can the form save valid values so users don't have to type all of them in again?
+        
+    exit();  // is this necessary?
+}
+
+
+//username validation - VARCHAR(20), no whitespace, does not exist in database
+
+$query = "SELECT accountUsername FROM Account WHERE accountUsername = '$username'";
+$result = mysqli_query($conn, $query);
+
+if (mb_strlen($username) > 20 || mb_strlen($username) < 4 || $username != trim($username)) {
+    $error = "Invalid input, username must be 4 to 20 characters long with no space, please try again.";
+    header("Location: register.php?error =" . urlencode ($error));
+    exit();
+} elseif (mysqli_num_rows($result) > 0) {
+    $error = "Username already exists, please try again.";
+    header("Location: register.php?error =" . urlencode ($error));
+    exit();
+} else {
+    printf("valid!");
+    exit();
+}
+
+
+// password confirmation - password == retyped-password
+if ($password != $passwordConfirmation) {
+    $error = "Your re-typed password is different";
+    header("Location: register.php?error =" . urlencode ($error));
+
+    // add a closable alert message box
+        
+    exit();  
+}
+
+
+
+// test connection by inserting data -- success
+// $query = "INSERT INTO Account (accountUsername,accountPassword,accountType,firstName,lastName,emailAddress,phoneNumber)
+// VALUES ('acc5','123','seller','john','doe','123@gmail.com','333333333')";
+// if (mysqli_query($conn, $query)) {
+//     echo "New record created successfully";
+// } else {
+//     echo "Error: " . $query . "<br>" . mysqli_error($conn);
+// }
+
+
+// create an account
+//$query = "INSERT INTO Account (accountUsername,accountPassword,accountType,firstName,lastName,emailAddress,phoneNumber)
+// VALUES ('$username', '$password', '$accountType', '$firstName', '$lastName', '$email', '$phoneNumber')";
+// if (mysqli_query($conn, $query)) {
+//     echo "New account created successfully";
+// } else {
+//     echo "Error: " . $query . "<br>" . mysqli_error($conn);
+// }
+    
 ?>
--- register.php
+++ register.php
@@ -19,29 +19,64 @@
       <small id="accountTypeHelp" class="form-text-inline text-muted"><span class="text-danger">* Required.</span></small>
 	</div>
   </div>
+
   <div class="form-group row">
-    <label for="email" class="col-sm-2 col-form-label text-right">Email</label>
+    <label for="username" class="col-sm-2 col-form-label text-right">Username</label>
 	<div class="col-sm-10">
-      <input type="text" class="form-control" id="email" placeholder="Email">
-      <small id="emailHelp" class="form-text text-muted"><span class="text-danger">* Required.</span></small>
+      <input type="text" class="form-control" name="username" id="username" placeholder="Username">
+      <small id="usernameHelp" class="form-text text-muted"><span class="text-danger">* Required.</span></small>
 	</div>
   </div>
+
   <div class="form-group row">
     <label for="password" class="col-sm-2 col-form-label text-right">Password</label>
     <div class="col-sm-10">
-      <input type="password" class="form-control" id="password" placeholder="Password">
+      <input type="password" class="form-control" name="password" id="password" placeholder="Password">
       <small id="passwordHelp" class="form-text text-muted"><span class="text-danger">* Required.</span></small>
     </div>
   </div>
   <div class="form-group row">
     <label for="passwordConfirmation" class="col-sm-2 col-form-label text-right">Repeat password</label>
     <div class="col-sm-10">
-      <input type="password" class="form-control" id="passwordConfirmation" placeholder="Enter password again">
+      <input type="password" class="form-control"name="passwordConfirmation" id="passwordConfirmation" placeholder="Enter password again">
       <small id="passwordConfirmationHelp" class="form-text text-muted"><span class="text-danger">* Required.</span></small>
     </div>
   </div>
+
   <div class="form-group row">
-    <button type="submit" class="btn btn-primary form-control">Register</button>
+    <label for="fisrtName" class="col-sm-2 col-form-label text-right">First name</label>
+	<div class="col-sm-10">
+      <input type="text" class="form-control" name="firstName" id="firstName" placeholder="First name">
+      <small id="firstnameHelp" class="form-text text-muted"><span class="text-danger">* Required.</span></small>
+	</div>
+  </div>
+
+  <div class="form-group row">
+    <label for="lastName" class="col-sm-2 col-form-label text-right">Last name</label>
+	<div class="col-sm-10">
+      <input type="text" class="form-control" name="lastName" id="lastName" placeholder="Last name">
+      <small id="lastnameHelp" class="form-text text-muted"><span class="text-danger">* Required.</span></small>
+	</div>
+  </div>
+
+  <div class="form-group row">
+    <label for="email" class="col-sm-2 col-form-label text-right">Email</label>
+	<div class="col-sm-10">
+      <input type="text" class="form-control" name="email" id="email" placeholder="Email">
+      <small id="emailHelp" class="form-text text-muted"><span class="text-danger">* Required.</span></small>
+	</div>
+  </div>
+  
+  <div class="form-group row">
+    <label for="phoneNumber" class="col-sm-2 col-form-label text-right">Phone number</label>
+	<div class="col-sm-10">
+      <input type="text" class="form-control" name="phoneNumber" id="phoneNumber" placeholder="Phone number">
+      <small id="phoneHelp" class="form-text text-muted"><span class="text-danger">* Required.</span></small>
+	</div>
+  </div>
+
+  <div class="form-group row">
+    <button type="submit"name="submit" class="btn btn-primary form-control">Register</button>
   </div>
 </form>
 
