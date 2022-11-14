20abe9a5fb302f6699299cbafb0cc98b1c5d4538
angellam99 <115711996+angellam99@users.noreply.github.com>
2022-11-14 11:17:03 +0000

    ?

--- register.php
+++ register.php
@@ -1,18 +1,9 @@
 <?php include_once("header.php")?>
 
-<<<<<<< Updated upstream
-<?php
-session_start();
-
-
-=======
-<<<<<<< Updated upstream
-=======
 <?php
 session_start();
 
 // oops, i used an alternative method than $_GET['error'], is it ok
->>>>>>> Stashed changes
 if(isset($_SESSION["alert"])) {
   $alert = $_SESSION["alert"];
   echo
@@ -26,11 +17,6 @@ unset($_SESSION["alert"]);  // alert disappears after refreshing
 }
 ?>
 
-
-<<<<<<< Updated upstream
-=======
->>>>>>> Stashed changes
->>>>>>> Stashed changes
 <div class="container">
 <h2 class="my-3">Register new account</h2>
 <!-- <h6 id="AccountRegHelp" class="form-text-inline text-muted" style="line-height:20px"><span class="text-danger">* All details are required.</span></h6> -->
@@ -62,64 +48,34 @@ unset($_SESSION["alert"]);  // alert disappears after refreshing
   <div class="form-group row">
     <label for="username" class="col-sm-2 col-form-label text-right">Username</label>
 	<div class="col-sm-10">
-<<<<<<< Updated upstream
-      <input type="text" class="form-control" name="username" id="username" value="<?php echo $_SESSION["username"];?>" placeholder="Username">
-      <?php 
-      if(empty($_SESSION["username"]) || ctype_space($_SESSION["username"])) {
-=======
-<<<<<<< Updated upstream
-      <input type="text" class="form-control" id="email" placeholder="Email">
-      <small id="emailHelp" class="form-text text-muted"><span class="text-danger">* Required.</span></small>
-	</div>
-=======
       <input type="text" class="form-control" name="username" id="username" value="<?php echo $_SESSION["username"];?>" placeholder="Username" required>
       <?php 
       if(!($_SESSION["username"]) || ctype_space($_SESSION["username"])) {
->>>>>>> Stashed changes
         echo '<small id="usernameHelp" class="form-text text-muted"><span class="text-danger">* Required. Must be 4 to 20 characters long, consists of only alphanumeric characters (A-Z, a-z, and 0-9), and has no space.</span></small>';
       }
       ?>
     </div>
-<<<<<<< Updated upstream
-=======
->>>>>>> Stashed changes
->>>>>>> Stashed changes
   </div>
 
   <div class="form-group row">
     <label for="password" class="col-sm-2 col-form-label text-right">Password</label>
     <div class="col-sm-10">
-<<<<<<< Updated upstream
       <input type="password" class="form-control" name="password" id="password" placeholder="Password">
       <small id="passwordHelp" class="form-text text-muted"><span class="text-danger">
         * Required. Must be 8 to 20 characters long, and must contain at least 1 letter and 1 number. 
         May contain any of these characters: !@#$%</span></small>
-=======
-<<<<<<< Updated upstream
-      <input type="password" class="form-control" id="password" placeholder="Password">
-      <small id="passwordHelp" class="form-text text-muted"><span class="text-danger">* Required.</span></small>
-=======
       <input type="password" class="form-control" name="password" id="password" placeholder="Password" required>
       <small id="passwordHelp" class="form-text text-muted"><span class="text-danger">
         * Required. Must be 8 to 20 characters long, and must contain at least 1 letter and 1 number. 
         May contain any of these characters: !@#$%</span></small>
->>>>>>> Stashed changes
->>>>>>> Stashed changes
     </div>
   </div>
   <div class="form-group row">
     <label for="passwordConfirmation" class="col-sm-2 col-form-label text-right">Confirm password</label>
     <div class="col-sm-10">
-<<<<<<< Updated upstream
       <input type="password" class="form-control"name="passwordConfirmation" id="passwordConfirmation" placeholder="Enter password again">
       <small id="passwordConfirmationHelp" class="form-text text-muted"><span class="text-danger">* Required.</span></small>
         </div>
-=======
-<<<<<<< Updated upstream
-      <input type="password" class="form-control" id="passwordConfirmation" placeholder="Enter password again">
-      <small id="passwordConfirmationHelp" class="form-text text-muted"><span class="text-danger">* Required.</span></small>
-    </div>
-=======
       <input type="password" class="form-control"name="passwordConfirmation" id="passwordConfirmation" placeholder="Enter password again" required>
       <small id="passwordConfirmationHelp" class="form-text text-muted"><span class="text-danger">* Required.</span></small>
         </div>
@@ -171,8 +127,6 @@ unset($_SESSION["alert"]);  // alert disappears after refreshing
       }
       ?>
         </div>
->>>>>>> Stashed changes
->>>>>>> Stashed changes
   </div>
 
   <div class="form-group row">
