96264a6c54b5e2a0bd555245f784238b53765438
angellam99 <115711996+angellam99@users.noreply.github.com>
2022-11-14 11:36:34 +0000

    Register(almost done), create auction(basic)
    221114

--- process_registration.php
+++ process_registration.php
@@ -4,4 +4,197 @@
 // an account. Notify user of success/failure and redirect/give navigation 
 // options.
 
+session_start();
+
+include "connection.php";  // using local db for testing for now
+
+
+/////////////////////////////////////////////////////
+
+// Utility functions (move somewhere else later?)
+
+// redirects to register page and shows a closeable red alert box with relevant error message
+// see code in register.php
+function function_alert_register($error) {
+    $_SESSION["alert"] = $error;
+    header("Location: register.php?error =" . urlencode ($error));  // redirection to register.php
+}
+
+// redirects to login page after successful registration with green alert box indicating success
+// see code in browse.php (put there for now)
+function function_success_register($success_message) {
+    $_SESSION["reg_success"] = $success_message;
+// to prevent inputs appearing again when going to register.php again
+    unset($_SESSION["username"]);  // what if I wanna save username for login? hmmmm...
+    unset($_SESSION["firstName"]); 
+    unset($_SESSION["lastName"]); 
+    unset($_SESSION["email"]); 
+    unset($_SESSION["phoneNumber"]); 
+    header("Location: browse.php?success =" . urlencode ($success_message));  // redirection to browse.php
+}  // might change redirection to the login page if a separate login page is created 
+
+/////////////////////////////////////////////////////
+
+
+// Test connection
+if (!$conn) {
+    $error = "Connection error, please try again later.";
+    function_alert_register($error);
+    exit();
+}
+
+
+// Variable extraction
+if (isset($_POST["submit"])) {
+    $accountType = $_POST["accountType"];
+    $username = mysqli_real_escape_string ($conn, $_POST["username"]);
+    $password = mysqli_real_escape_string ($conn, $_POST["password"]);
+    $passwordConfirmation = mysqli_real_escape_string ($conn, $_POST["passwordConfirmation"]);
+    $firstName = mysqli_real_escape_string ($conn, $_POST["firstName"]);
+    $lastName = mysqli_real_escape_string ($conn, $_POST["lastName"]);
+    $email = mysqli_real_escape_string ($conn, $_POST["email"]);
+    $phoneNumber = mysqli_real_escape_string ($conn, $_POST["phoneNumber"]);
+} 
+// alternative data cleaning: trim() then stripslashes() then htmlspecialchars()
+
+
+// temporarily saves data in form input field so users don't have to enter all over again
+// password and confirmation are never saved
+// see changes to code in form in register.php
+$_SESSION["account_type"] = $accountType;  // sorry for confusing naming styles, this is to match header.php
+$_SESSION["username"] = $username;
+$_SESSION["firstName"] = $firstName;
+$_SESSION["lastName"] = $lastName;
+$_SESSION["email"] = $email;
+$_SESSION["phoneNumber"] = $phoneNumber;
+
+// test
+// printf($username);
+// echo "<br>";
+// printf($password);
+// echo "<br>";
+// printf($passwordConfirmation);
+// echo "<br>";
+// printf($firstName);
+// echo "<br>";
+// printf($lastName);
+// echo "<br>";
+// printf($email);
+// echo "<br>";
+// printf($phoneNumber);
+// echo "<br>";
+// printf($accountType);
+// echo "<br>";
+
+
+// Input validiation
+// what is the best structure/hierarchy?
+// current hierarchy is according to order below
+
+// check for empty input
+if (empty($username) || empty($password) || empty($passwordConfirmation) || empty($firstName) ||
+empty($lastName) || empty($email) || empty($phoneNumber) || ctype_space($firstName) || ctype_space($lastName)) {
+    $error = "Please fill in all the required details.";
+    function_alert_register($error);
+    exit();
+}
+// Question: separate checks for each input, or group them into one statement?
+
+
+// Username validation - 4 to 20 characters long, no whitespace, does not exist in database
+// Question: other extra validation? legal characters?
+
+$result = mysqli_query($conn, "SELECT accountUsername FROM Account WHERE accountUsername = '$username'");
+
+if (mb_strlen($username) > 20 || mb_strlen($username) < 4 || 
+strpos($username, ' ') != false || !ctype_alnum($username) || ctype_space($username)) {
+    unset($_SESSION["username"]); 
+    $error = "Invalid username format, please try again.";
+
+    // error message ver1: javascript alert pop up --> redirect to register.php
+    // echo "<script type='text/javascript'>alert('$error');</script>";  // popup box
+    // how to change "localhost says"?
+
+    // error message ver2: HTML alert message box and sessions
+    // see code in register.php
+    function_alert_register($error);
+    exit();
+} elseif (mysqli_num_rows($result) > 0) {  // query finds same username in database
+    unset($_SESSION["username"]); 
+    $error = "Username already exists, please try again.";
+    function_alert_register($error);
+    exit(); 
+} 
+// no need to exit if input is valid 
+
+
+// Password validation - must be 8 to 20 characters long, no space
+// Question: other extra validation? legal characters?
+if (mb_strlen($password) > 20 || mb_strlen($password) < 8 || 
+strpos($password, ' ') != false || !preg_match("/^(?=.*\d)(?=.*[A-Za-z])[0-9A-Za-z!@#$%]{8,20}$/", $password)) {
+    $error = "Invalid password format, please try again.";
+    function_alert_register($error);
+    exit();
+}
+
+// Password confirmation - password == retyped-password
+if ($password != $passwordConfirmation) {
+    $error = "The password confirmation does not match, please try again.";
+    function_alert_register($error);
+    exit();  
+}
+
+// Hash password to protect it, save hash to database instead of the actual password
+// See tutorial 4
+// Verify using password_verify ($password, $hash) function
+$hash = password_hash($password, PASSWORD_DEFAULT);  // requires VARCHAR(60) in database
+
+
+// Question: First name and last name validations necessary? Requirements?
+
+
+// Email validation -- can only validate format for now
+$email = filter_var($email, FILTER_SANITIZE_EMAIL);
+if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    unset($_SESSION["email"]); 
+    $error = "Invalid email format, please try again.";
+    function_alert_register($error);
+    exit();   
+}
+// Question: extra: email confirmation? lol
+
+
+// Phone format validation - start with + sign, 7-15 numbers (not including + sign) (I set the length arbituarily)
+$phoneNumber = str_replace([" ", ".", "-", "(", ")"], "", $phoneNumber);
+if (!preg_match("/[+][0-9]{7,15}/", $phoneNumber)) {
+    unset($_SESSION["phoneNumber"]); 
+    $error = "Invalid phone format, please try again.";
+    function_alert_register($error);
+    exit(); 
+}
+
+
+// test connection by inserting data directly
+// $query = "INSERT INTO Account (accountUsername,accountPassword,accountType,firstName,lastName,emailAddress,phoneNumber)
+// VALUES ('acc5','12345678','seller','John','Doe','123@abc.com','+3333333333')";
+// if (mysqli_query($conn, $query)) {
+//     echo "New record created successfully";
+// } else {
+//     echo "Error: " . $query . "<br>" . mysqli_error($conn);
+// }
+
+
+// Create an account!
+$query = "INSERT INTO Account (accountUsername, accountPassword, accountType, firstName,lastName, emailAddress, phoneNumber)
+VALUES ('$username', '$hash', '$accountType', '$firstName', '$lastName', '$email', '$phoneNumber')";
+if (mysqli_query($conn, $query)) {
+    mysqli_close($conn);  // put this here?
+    $success_message = "Account created successfully.";
+    function_success_register($success_message);
+} else {
+   // echo "Error: " . $query . "<br>" . mysqli_error($conn);
+    $error = "Connection error, please try again later.";
+    function_alert_register($error);
+}
+
 ?>
--- register.php
+++ register.php
@@ -1,7 +1,32 @@
 <?php include_once("header.php")?>
 
+<?php
+session_start();
+
+// oops, i used an alternative method than $_GET['error'], is it ok
+if(isset($_SESSION["alert"])) {
+  $alert = $_SESSION["alert"];
+  echo
+
+  "<div class='alert' style='background-color:pink; color:black'>
+  <button type='button' class='close' data-dismiss='alert'>&times;</button>
+  <h5><i class='icon fa fa-close'></i> Error</h5>$alert
+  </div>";
+  
+unset($_SESSION["alert"]);  // alert disappears after refreshing
+}
+?>
+
 <div class="container">
 <h2 class="my-3">Register new account</h2>
+<!-- <h6 id="AccountRegHelp" class="form-text-inline text-muted" style="line-height:20px"><span class="text-danger">* All details are required.</span></h6> -->
+
+
+<!-- Note: this form is modified:
+- a lot of the inputs did not have a name 
+- user can keep form input field values before successful registration
+- separate alerts depending on input
+-->
 
 <!-- Create auction form -->
 <form method="POST" action="process_registration.php">
@@ -16,32 +41,146 @@
         <input class="form-check-input" type="radio" name="accountType" id="accountSeller" value="seller">
         <label class="form-check-label" for="accountSeller">Seller</label>
       </div>
-      <small id="accountTypeHelp" class="form-text-inline text-muted"><span class="text-danger">* Required.</span></small>
+      <small id="accountTypeHelp" class="form-text-inline text-muted"><span class="text-danger">* One account can only have one role.</span></small>
 	</div>
   </div>
+
   <div class="form-group row">
-    <label for="email" class="col-sm-2 col-form-label text-right">Email</label>
+    <label for="username" class="col-sm-2 col-form-label text-right">Username</label>
 	<div class="col-sm-10">
-      <input type="text" class="form-control" id="email" placeholder="Email">
-      <small id="emailHelp" class="form-text text-muted"><span class="text-danger">* Required.</span></small>
-	</div>
+      <input type="text" class="form-control" name="username" id="username" value="<?php echo $_SESSION["username"];?>" placeholder="Username" required>
+      <?php 
+      if(!($_SESSION["username"]) || ctype_space($_SESSION["username"])) {
+        echo '<small id="usernameHelp" class="form-text text-muted"><span class="text-danger">* Required. Must be 4 to 20 characters long, consists of only alphanumeric characters (A-Z, a-z, and 0-9), and has no space.</span></small>';
+      }
+      ?>
+    </div>
   </div>
+
   <div class="form-group row">
     <label for="password" class="col-sm-2 col-form-label text-right">Password</label>
     <div class="col-sm-10">
-      <input type="password" class="form-control" id="password" placeholder="Password">
-      <small id="passwordHelp" class="form-text text-muted"><span class="text-danger">* Required.</span></small>
+      <input type="password" class="form-control" name="password" id="password" placeholder="Password">
+      <small id="passwordHelp" class="form-text text-muted"><span class="text-danger">
+        * Required. Must be 8 to 20 characters long, and must contain at least 1 letter and 1 number. 
+        May contain any of these characters: !@#$%</span></small>
+      <input type="password" class="form-control" name="password" id="password" placeholder="Password" required>
+      <small id="passwordHelp" class="form-text text-muted"><span class="text-danger">
+        * Required. Must be 8 to 20 characters long, and must contain at least 1 letter and 1 number. 
+        May contain any of these characters: !@#$%</span></small>
     </div>
   </div>
   <div class="form-group row">
-    <label for="passwordConfirmation" class="col-sm-2 col-form-label text-right">Repeat password</label>
+    <label for="passwordConfirmation" class="col-sm-2 col-form-label text-right">Confirm password</label>
     <div class="col-sm-10">
-      <input type="password" class="form-control" id="passwordConfirmation" placeholder="Enter password again">
+      <input type="password" class="form-control"name="passwordConfirmation" id="passwordConfirmation" placeholder="Enter password again">
       <small id="passwordConfirmationHelp" class="form-text text-muted"><span class="text-danger">* Required.</span></small>
-    </div>
+        </div>
+      <input type="password" class="form-control"name="passwordConfirmation" id="passwordConfirmation" placeholder="Enter password again" required>
+      <small id="passwordConfirmationHelp" class="form-text text-muted"><span class="text-danger">* Required.</span></small>
+        </div>
   </div>
+
   <div class="form-group row">
-    <button type="submit" class="btn btn-primary form-control">Register</button>
+    <label for="fisrtName" class="col-sm-2 col-form-label text-right">First name</label>
+	<div class="col-sm-10">
+      <input type="text" class="form-control" name="firstName" id="firstName"value="<?php echo $_SESSION["firstName"];?>" placeholder="First name" required>
+      <?php 
+      if(!($_SESSION["firstName"]) || ctype_space($_SESSION["firstName"])) {
+        echo '<small id="firstnameHelp" class="form-text text-muted"><span class="text-danger">* Required.</span></small>';
+      }
+      ?>
+        </div>
+  </div>
+
+  <div class="form-group row">
+    <label for="lastName" class="col-sm-2 col-form-label text-right">Last name</label>
+	<div class="col-sm-10">
+      <input type="text" class="form-control" name="lastName" id="lastName" value="<?php echo $_SESSION["lastName"];?>" placeholder="Last name" required>
+      <?php 
+      if(!($_SESSION["lastName"]) || ctype_space($_SESSION["lastName"])) {
+        echo '<small id="lastnameHelp" class="form-text text-muted"><span class="text-danger">* Required.</span></small>';
+      }
+      ?>
+        </div>
+  </div>
+
+  <div class="form-group row">
+    <label for="email" class="col-sm-2 col-form-label text-right">Email</label>
+	<div class="col-sm-10">
+      <input type="email" class="form-control" name="email" id="email" value="<?php echo $_SESSION["email"];?>" placeholder="Email" required>
+      <?php 
+      if(!isset($_SESSION["email"]) || ctype_space($_SESSION["email"])) {
+        echo '<small id="emailHelp" class="form-text text-muted"><span class="text-danger">* Required.</span></small>';
+      }
+      ?>
+	</div>
+  </div>
+  
+  <div class="form-group row">
+    <label for="phoneNumber" class="col-sm-2 col-form-label text-right">International phone number</label>
+	<div class="col-sm-10">
+      <input type="text" class="form-control" name="phoneNumber" id="phoneNumber" value="<?php echo $_SESSION["phoneNumber"];?>" placeholder="International phone number" required>
+      <?php 
+      if (!isset($_SESSION["phoneNumber"]) || ctype_space($_SESSION["phoneNumber"])) {
+        echo '<small id="phoneHelp" class="form-text text-muted"><span class="text-danger">* Please start with + sign (not 00) and country code.</span></small>';
+      }
+      ?>
+        </div>
+  </div>
+
+  <div class="form-group row">
+    <label for="fisrtName" class="col-sm-2 col-form-label text-right">First name</label>
+	<div class="col-sm-10">
+      <input type="text" class="form-control" name="firstName" id="firstName"value="<?php echo $_SESSION["firstName"];?>" placeholder="First name">
+      <?php 
+      if(empty($_SESSION["firstName"]) || ctype_space($_SESSION["firstName"])) {
+        echo '<small id="firstnameHelp" class="form-text text-muted"><span class="text-danger">* Required.</span></small>';
+      }
+      ?>
+        </div>
+  </div>
+
+  <div class="form-group row">
+    <label for="lastName" class="col-sm-2 col-form-label text-right">Last name</label>
+	<div class="col-sm-10">
+      <input type="text" class="form-control" name="lastName" id="lastName" value="<?php echo $_SESSION["lastName"];?>" placeholder="Last name">
+      <?php 
+      if(empty($_SESSION["lastName"]) || ctype_space($_SESSION["lastName"])) {
+        echo '<small id="lastnameHelp" class="form-text text-muted"><span class="text-danger">* Required.</span></small>';
+      }
+      ?>
+        </div>
+  </div>
+
+  <div class="form-group row">
+    <label for="email" class="col-sm-2 col-form-label text-right">Email</label>
+	<div class="col-sm-10">
+      <input type="email" class="form-control" name="email" id="email" value="<?php echo $_SESSION["email"];?>" placeholder="Email">
+      <?php 
+      if(empty($_SESSION["email"]) || ctype_space($_SESSION["email"])) {
+        echo '<small id="emailHelp" class="form-text text-muted"><span class="text-danger">* Required.</span></small>';
+      }
+      ?>
+	</div>
+  </div>
+  
+  <div class="form-group row">
+    <label for="phoneNumber" class="col-sm-2 col-form-label text-right">International phone number</label>
+	<div class="col-sm-10">
+      <input type="text" class="form-control" name="phoneNumber" id="phoneNumber" value="<?php echo $_SESSION["phoneNumber"];?>" placeholder="International phone number">
+      <?php 
+      if(empty($_SESSION["phoneNumber"])) {
+        echo '<small id="phoneHelp" class="form-text text-muted"><span class="text-danger">* Required. Please start with + sign (not 00) and country code.</span></small>';
+      } elseif (!isset($_SESSION["phoneNumber"]) || ctype_space($_SESSION["phoneNumber"])) {
+        echo '<small id="phoneHelp" class="form-text text-muted"><span class="text-danger">* Please start with + sign (not 00) and country code.</span></small>';
+      }
+      ?>
+        </div>
+  </div>
+
+  <div class="form-group row">
+    <button type="submit"name="submit" class="btn btn-primary form-control">Register</button>
   </div>
 </form>
 
