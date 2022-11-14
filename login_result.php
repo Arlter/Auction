6ad66adcb5fd16fd100d9830c13dc1b4c18c05bf
kienmd <110916296+kienmd@users.noreply.github.com>
2022-11-14 10:51:20 +0000

    Updated login_result.php

--- login_result.php
+++ login_result.php
@@ -1,22 +1,24 @@
 <?php
 // TODO: Extract $_POST variables, check they're OK, and attempt to login.
 // Notify user of success/failure and redirect/give navigation options.
-
 // For now, I will just set session variables and redirect.
-
 // assign post to 
+//Go on to header.php. Under Login should send request to login_result.php
+require_once "connection.php";
 session_start();
-$_SESSION['logged_in'] = True;
-$_SESSION['username'] = "acc1"; 
-$_SESSION['account_type'] = "seller";
-$_SESSION['accountID'] = 10000000; // get from post form 
-$_SESSION['emailAddress'] = "1419121502@qq.com";
+$email = $_POST["email"];
+    //echo "123123";
+$pass = $_POST["password"];
 
+$rows = mysqli_query($conn, "SELECT * FROM Account WHERE emailAddress = '$email' and accountPassword = '$pass' ")-> fetch_array(MYSQLI_NUM);
+if(isset($rows)){
+        echo('<div class="text-center">You are now logged in! You will be redirected shortly.</div>');
+        // Redirect to index after 5 seconds
+        header("refresh:3;url=index.php");
+        } else {
+        echo "asjfad";
+        header("refresh:3;url=index.php");
+        }
 
-
-echo('<div class="text-center">You are now logged in! You will be redirected shortly.</div>');
-
-// Redirect to index after 5 seconds
-header("refresh:5;url=index.php");
-
+?>
 ?>
