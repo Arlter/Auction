226fef20d3352979879d45d643d7366252acef71
angellam99 <115711996+angellam99@users.noreply.github.com>
2022-11-24 04:02:20 +0000

    Update logout.php

--- logout.php
+++ logout.php
@@ -3,6 +3,7 @@ include_once("header.php");
 unset($_SESSION['accountType']);
 unset($_SESSION['accountID']);
 unset($_SESSION['emailAddress']);
+unset($_SESSION["accountUsername"]);
 setcookie(session_name(), "", time() - 360);
 //session_destroy();
 $_SESSION['logged_in'] = 0;
