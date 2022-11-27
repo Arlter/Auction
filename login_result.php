fe772844a42b792a07b24ccb0c4f5ca10c8dd10b
Arlter <38512367+Arlter@users.noreply.github.com>
2022-11-27 20:22:38 +0000

    Update login_result.php

--- login_result.php
+++ login_result.php
@@ -51,10 +51,10 @@ if (password_verify($pass, $hash)) {  // returns true if the password and hash m
         echo('<div class="text-center" style="margin-top:50px">Login error, please try again. You will be redirected shortly.</div>');
         exit();  
     }
-    // echo "<script>
-    // window.location.href = 'https://auction-php.azurewebsites.net/browse.php';
-    // </script>";
-    header("location:https://auction-php.azurewebsites.net/browse.php");
+     echo "<script>
+     window.location.href = 'https://auction-php.azurewebsites.net/browse.php';
+     </script>";
+
     echo('<div class="text-center" style="margin-top:50px">You are now logged in! You will be redirected shortly.</div>');
     exit();  
 } else {
