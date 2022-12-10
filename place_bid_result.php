96f4b7428a179eb002ea1a39a148d80509928892
Arlter <38512367+Arlter@users.noreply.github.com>
2022-12-10 22:17:29 +0000

    Update place_bid_result.php

--- place_bid_result.php
+++ place_bid_result.php
@@ -23,6 +23,15 @@
     $accountId= $_SESSION['accountID'];
     $email_address=$_SESSION['emailAddress'];
 
+    // Verify if there exists an outbidder
+    $cur_bidder_res = mysqli_query($conn, "SELECT currentBidder FROM Auction WHERE auctionID = $auctionId");
+    if (mysqli_num_rows($cur_bidder_res)>0){
+        $cur_bidder= $cur_bidder_res -> fetch_array(MYSQLI_NUM)[0];
+        if ( !is_null($cur_bidder)){
+            $outbidder_email = mysqli_query($conn, "SELECT emailAddress FROM Account WHERE accountID = $cur_bidder")-> fetch_array(MYSQLI_NUM)[0];
+        }
+    }
+   
     $query = "INSERT INTO Bid(auction_auctionID,bidPrice,buyer_accountID,bidTime) VALUES($auctionId,CAST($bidPrice AS DECIMAL(10,2)), $accountId, CURRENT_TIME())";
     $res = mysqli_query($conn, $query);
     if (mysqli_affected_rows($conn) ==1 && !mysqli_error($conn))
@@ -34,15 +43,29 @@
         $email_subject = "[Buyer Notification] Successful bid on the auction : ".$auctionId;
         $email_content = " Congratulation! You have placed a bid of ".chr(163). $bidPrice." on the auction ".$auctionId.$auction_url;
         send_email($email_address,$email_subject,$email_content);
+        // Watcher notification
         $watcher_email_subject = "[Watcher Notification] A new bid ".chr(163).$bidPrice." on the auction ".$auctionId." has been made.";
         $watcher_email_content = "A new bid of price ".$bidPrice." has been made to the auction ".$auctionId.$auction_url;
         $watcher_email_address = mysqli_query($conn, "SELECT emailAddress FROM Account, BuyerWatchAuction WHERE accountID=buyer_accountID and auction_auctionID=$auctionId");  
         if (mysqli_num_rows($watcher_email_address)>0) {
-            while( $watcher_email = $watcher_email_address -> fetch_array(MYSQLI_NUM)){
-                if ($watcher_email[0] != $email_address){
-                    send_email($watcher_email[0], $watcher_email_subject, $watcher_email_content);
+            if (isset($outbidder_email)){
+                // Outbid notification
+                $outbidder_email_subject = "[Outbid Notification] A higher bid ".chr(163).$bidPrice." on the auction ".$auctionId." has been made.";
+                $outbidder_email_content = "We are sorry to inform you that a higher bid of price ".$bidPrice." has been made to the auction ".$auctionId.$auction_url;
+                send_email($outbidder_email, $outbidder_email_subject, $outbidder_email_content);
+                while( $watcher_email = $watcher_email_address -> fetch_array(MYSQLI_NUM)){
+                    if ($watcher_email[0] != $email_address && $watcher_email[0] != $outbidder_email){
+                        send_email($watcher_email[0], $watcher_email_subject, $watcher_email_content);
+                    }
+                }
+            }else{
+                while( $watcher_email = $watcher_email_address -> fetch_array(MYSQLI_NUM)){
+                    if ($watcher_email[0] != $email_address){
+                        send_email($watcher_email[0], $watcher_email_subject, $watcher_email_content);
+                    }
                 }
             }
+
         }
     }
 
